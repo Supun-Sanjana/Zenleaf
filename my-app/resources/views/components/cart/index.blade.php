cc925fe520d383f39c80e28b3cf40dc5e770b9ef
Supun Sanjana <infor.ssupun@gmail.com>
2025-09-11 16:15:09 +0530

    initialize payhere to checkout

--- my-app/resources/views/components/cart/index.blade.php
+++ my-app/resources/views/components/cart/index.blade.php
@@ -10,12 +10,31 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-1h6G3D..." crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+
+
 </head>
 
 <body>
     <x-discover.discover-header class="mb-20" />
+    <a href="{{ route('discover') }}">
+        <div class="mt-20 flex items-center gap-4 mb-5">
+        <h2 class="text-xl font-semibold flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="lucide lucide-chevron-left mr-2">
+                <path d="m15 18-6-6 6-6" />
+            </svg>
+            Shopping Continue
+        </h2>
 
-    <h1 class="mt-20 text-emerald-500 text-2xl font-semibold mb-2 px-4">Your Cart</h1>
+    </div>
+    </a>
+    
+    <hr class="flex-grow border-t border-gray-300" />
+
+
+
+    <h1 class=" mt-5 text-emerald-500 text-2xl font-semibold mb-2 px-4">Shopping cart</h1>
 
     @if(count($cart) > 0)
         @foreach($cart as $id => $item)
@@ -23,9 +42,9 @@
                 <div class="p-4 flex justify-between items-center">
                     <div>
                         <h3 class="text-2xl">{{ $item['name'] }}</h3>
-                     <h3 class="text-md">${{ $item['price'] }}</h3>
+                        <h3 class="text-md">${{ $item['price'] }}</h3>
                     </div>
-                    
+
 
                     <div>
                         <form action="{{ route('cart.update', $id) }}" method="POST">
@@ -35,7 +54,7 @@
                             <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Update</button>
                         </form>
                     </div>
-                   
+
 
                     <form action="{{ route('cart.remove', $id) }}" method="POST">
                         @csrf
@@ -49,12 +68,15 @@
             <div>
                 <h2 class="text-2xl mb-2">Total: ${{ $total }}</h2>
 
-            <form action="{{ route('cart.checkout') }}" method="POST">
-                @csrf
-                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Checkout</button>
-            </form>
+                <form action="{{ route('cart.checkout') }}" method="POST">
+                    @csrf
+                    <a href="">
+                        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Checkout</button>
+                    </a>
+                    
+                </form>
             </div>
-            
+
         </div>
 
 
